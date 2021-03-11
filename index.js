4d0a9eefd6fbe1b4291a46aa872257cbd5a4d3c9
Ignacio-Ramirez <80483829+Ignacio-Ramirez@users.noreply.github.com>
2021-03-11 17:14:35 -0300

    Hide Token

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const token = process.env.TOKEN;
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('NzcwOTY0NDMzNzg2MzcyMTA2.X5lOlA.rqGkTqHkQXwpOsWbSW4eEKUXTyo');
+client.login(token);
