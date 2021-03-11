512d4326008c2f30369d45268a2da8964e3dfcdb
Ignacio-Ramirez <80483829+Ignacio-Ramirez@users.noreply.github.com>
2021-03-11 17:09:17 -0300

    Initial Version

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NzcwOTY0NDMzNzg2MzcyMTA2.X5lOlA.rqGkTqHkQXwpOsWbSW4eEKUXTyo');
